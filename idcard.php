af5a1c2cd8e8d91dafa220d1dc8d4088576f5fcd
FritsvanLatum <f.a.vanlatum@vlfg.nl>
2018-12-11 16:31:47 +0100

    idcards page

--- idcard.php
+++ idcard.php
@@ -1,15 +1,16 @@
 <?php
 require_once './patron/key_idm.php';
 require_once './patron/patron.php';
-
+require_once './vendor/autoload.php';
+$template_file = './patron/user_template.html';
 $patron_barcode = null;
-if (array_key_exists('patron',$_POST)) {
+$patron = new Patron($config_idm['wskey'], $config_idm['secret'], $config_idm['ppid']);
+if (array_key_exists('patronBarcode',$_POST)) {
 
 
-  $patron_barcode = $_POST['patron'];
+  $patron_barcode = $_POST['patronBarcode'];
 
   //get patron
-  $patron = new Patron($config_idm['wskey'], $config_idm['secret'], $config_idm['ppid']);
   $search = '{"schemas": ["urn:ietf:params:scim:api:messages:2.0:SearchRequest"], '.
   '"filter": "External_ID eq \"'.$patron_barcode.'\""}';
   $patron->search_patron($search);
@@ -17,7 +18,7 @@ if (array_key_exists('patron',$_POST)) {
   //analyze result
   if ($patron->search['totalResults'] == 0) {
     //not found
-    
+
   }
   else {
     if ($patron->search['totalResults'] > 1){
@@ -35,15 +36,36 @@ if (array_key_exists('patron',$_POST)) {
   <head>
     <title>ID cards</title>
     <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/idcard.css">
   </head>
 
   <body>
+    <div class="form">
+      <form action="" method="post">
 
-    <form action="" method="post">
+        Patron barcode: <input id="patronBarcode" type="text" name="patronBarcode" <?php if ($patron_barcode) echo 'value="'.$patron_barcode.'"' ; ?> /> <br />
+        <input id="submit" type="submit" value="Submit">
+        <input type="reset" value="Reset">
+      </form>
+    </div>
 
-      Patron barcode: <input type="text" name="patron" <?php if ($patron_barcode) echo 'value="'.$patron_barcode.'"' ; ?> /> <br />
-      <input type="submit" value="Submit">
-    </form>
+    <?php if ($patron->search['totalResults'] > 0) {
+      $tel = 0;
+      foreach ($patron->search['Resources'] as $resource) {
+        $tel++;
+        //
+        $resource['teller'] = $tel;
+
+        $loader = new Twig_Loader_Filesystem(__DIR__);
+        $twig = new Twig_Environment($loader, array(
+        //specify a cache directory only in a production setting
+        //'cache' => './compilation_cache',
+        ));
+
+        echo $twig->render($template_file, $resource);
+      }
+    }
+    ?>
     <div>
       Search:
       <pre>
