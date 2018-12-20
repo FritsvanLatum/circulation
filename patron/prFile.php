052e3dc1837c6da9b5f19f757b20796793eee674
FritsvanLatum <f.a.vanlatum@vlfg.nl>
2018-12-20 16:24:15 +0100

    printing idcards

--- patron/prFile.php
+++ patron/prFile.php
@@ -2,16 +2,23 @@
 
 $pdf_file = $_GET['file'];
 
+$msg = "";
+
 if (file_exists($pdf_file)) {
   //try to print when pdf_file exists
   $retval = 1;
   $last_line = exec('lp '.$pdf_file.' -d balie',$output, $retval);
   //$retval = 0;
   //move html file and delete pdf file if command succeeded
+  $msg = "$retval - $output -- $last_line\n"
   if ($retval == 0) {
     $deleted = unlink($pdf_file);
   }
 }
+else {
+  $msg = "File $pdf_file not found.\n"
+}
 
+echo $msg;
 
 ?>
