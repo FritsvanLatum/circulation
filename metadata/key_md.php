5f67c3ade36d90e5035586b8e72a4d4758b9d826
FritsvanLatum <f.a.vanlatum@vlfg.nl>
2019-01-10 14:36:41 +0100

    note verplaatst

---
+++ metadata/key_md.php
@@ -0,0 +1,13 @@
+<?php
+//idm config
+
+$config_idm = [];
+
+//wskey en secret van PPL-APIkey
+$config_idm['wskey'] = "tA2yibd4vmeqTt7TYEmYNOzLHUWEGGF07v3dlR8YnvQni9wI0DLpBbARZtOZXpDsCFELxwV202smYFx8";
+$config_idm['secret'] = "akpDcRzATqHZakmw3b76bvJcIDrNqGKY";
+
+//ppid van patron "api metadata"
+$config_idm['ppid'] = "290bb343-c02e-44a0-9f8b-0ce04fbfc323"; // "348ee52b-d276-4609-b8ad-415af574d0c0";
+
+?>
